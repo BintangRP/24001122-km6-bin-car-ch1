b9ca06a1bc88243d1f181e8bce2e5e2017e10506
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-03-12 12:50:13 +0700

    ref: carousel js

---
+++ js/tiny-slider.js
@@ -0,0 +1,14 @@
+var slider = tns({
+    container: ".my-slider",
+    mode: "carousel",
+    items: 4,
+    mouseDrag: true,
+    center: true,
+    fixedWidth: 720,
+    gutter: 32,
+    controlsPosition: "bottom",
+    controlsContainer: "#tns-controls",
+    prevButton: ".prev-button",
+    nextButton: ".next-button",
+    nav: false
+});
