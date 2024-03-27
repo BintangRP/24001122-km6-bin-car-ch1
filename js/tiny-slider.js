955a400df67c18c2f92f513e5be1cdbb111984f5
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-03-28 00:57:23 +0700

    fix: tiny-slider testimonial

--- js/tiny-slider.js
+++ js/tiny-slider.js
@@ -1,15 +1,36 @@
 var slider = tns({
     container: ".my-slider",
     mode: "carousel",
-    items: 4,
     mouseDrag: true,
-    center: true,
     // autoWidth: true,
-    fixedWidth: 720,
-    gutter: 32,
+
+    responsive: {
+        1000: {
+            items: 4,
+            gutter: 32,
+            center: true,
+
+        },
+        500: {
+            items: 1,
+            fixedWidth: 720,
+            gutter: 32,
+            center: true,
+            loop: true,
+        },
+        0: {
+            items: 1,
+            center: true,
+            fixedWidth: 350,
+            gutter: 32,
+        },
+    },
+
     controlsPosition: "bottom",
     controlsContainer: "#tns-controls",
     prevButton: ".prev-button",
     nextButton: ".next-button",
-    nav: false
+    nav: false,
+    preventScrollOnTouch: 'auto'
+
 });
